f509e100260089205fe7f7d892a50a52ba0e5d5d
Andres Dike <dikegod12@gmail.com>
2026-08-08 11:48:48 -0500

    Modelo clase usuario creado

--- src/main/java/com/dike/calculadoraapi/model/Usuario.java
+++ src/main/java/com/dike/calculadoraapi/model/Usuario.java
@@ -1,4 +1,36 @@
 package com.dike.calculadoraapi.model;
 
 public class Usuario {
+    private String nombre;
+    private String correo;
+    private Long id;
+    public Usuario (String nombre, String correo, Long id){
+        this.correo=correo;
+        this.id=id;
+        this.nombre=nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
